1660b1db92386b39dff3cd2c52e3c2f523727cac
fie <alfie.machica@gmail.com>
2023-10-26 10:16:37 +0800

    test20231026

---
+++ p7.py
@@ -0,0 +1,6 @@
+import numpy as np
+# import math
+b = 5.2
+print (np.log(b))
+print (2.718281828459045 ** 1.6486586255873816)
+# print (math.e)
---
+++ p7B.py
@@ -0,0 +1,24 @@
+"""
+import numpy as np
+import math
+b = 5.2
+print (np.log(b))
+print (math.e ** 1.6486586255873816)
+# print (math.e)
+"""
+
+import math
+
+softmax_output = [0.7, 0.1, 0.2]
+target_output = [1, 0, 0]
+
+loss = -(math.log(softmax_output[0]) * target_output[0] +
+		math.log(softmax_output[1]) * target_output[1] +
+		math.log(softmax_output[2]) * target_output[2] )
+		
+print (loss)
+loss = -math.log(softmax_output[0])
+print (loss)
+
+print (-math.log(0.7))
+print (-math.log(0.5))
